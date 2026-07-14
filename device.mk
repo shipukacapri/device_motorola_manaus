e0fb27fa2fd148dfb8ff92033d69bf890a619652
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-07-14 17:59:26 +0530

    manaus: add VoltageOS maintainer overlay

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/motorola/manaus
 
+# Overlay Voltage Maintainer
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-voltage
+
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
